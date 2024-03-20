6ebce47659797663279d5a2e68c0c29f5eee86a0
Jonah Mwangala <jonahxnl@gmail.com>
2024-03-21 00:11:42 +0300

    Completed Lab

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback) {
+    callback()
+}
+function returnsANamedFunction(named) {
+    return function named() {}
+}
+function returnsAnAnonymousFunction() {
+    return function() {}
+}
